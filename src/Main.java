026f03b593cfad3cc2f3464b9732d9ddb53fdb54
AnArchoz <antoinerebelo94@gmail.com>
2019-02-16 02:07:25 +0100

    Added a filechooser for choosing directories instead of my earlier hardcode path.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.security.NoSuchAlgorithmException;
 
@@ -5,7 +7,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		Controller controller = new Controller();
-		String folderPath = "Your Folder To Analyze Here";
+		String folderPath = "";
+
+		// Make ugly swing look like sexy
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		JFileChooser fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+		int y = fc.showOpenDialog(null);
+
+		if (y == JFileChooser.APPROVE_OPTION) {
+			folderPath = fc.getSelectedFile().getAbsolutePath();
+		} else {
+			System.exit(1);
+		}
 
 		try {
 			long start = System.currentTimeMillis();
